6c0fda875600b1f46e1085b1fd33b932d3d6a27b
Elias Bernhaut <ebernhaut@itbernhaut.ch>
2017-01-18 14:19:49 +0100

    Created basic mockup

--- src/Main.elm
+++ src/Main.elm
@@ -1,35 +1,113 @@
 module Main exposing (..)
 
-import Html exposing (Html, button, div, text, h1)
+import Html exposing (Html, button, div, footer, header, section, article, text, h1, h2, hr, a, img)
+import Html.Attributes exposing (src, href)
 import Html.CssHelpers
+import Navigation
 import Style
 
 
 { id, class, classList } =
     Html.CssHelpers.withNamespace "bbs"
 
+
 main : Program Never Model Msg
 main =
-    Html.beginnerProgram { model = model, view = view, update = update }
+    Navigation.program UrlChange
+        { init = init
+        , view = view
+        , update = update
+        , subscriptions = (\_ -> Sub.none)
+        }
+
+
 
 -- MODEL
-type alias Model =
-    Int
 
-model : Model
-model = 0
+
+type alias Model =
+    { history : List Navigation.Location
+    }
+
+
+init : Navigation.Location -> ( Model, Cmd Msg )
+init location =
+    ( Model [ location ]
+    , Cmd.none
+    )
+
+
 
 -- UPDATE
-type Msg = Dummy
 
 
-update : Msg -> Model -> Model
-update msg model = model
+type Msg
+    = UrlChange Navigation.Location
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    case msg of
+        UrlChange location ->
+            ( { model | history = location :: model.history }
+            , Cmd.none
+            )
+
+
 
 -- VIEW
+
+
+postContent : Model -> Html Msg
+postContent model =
+    case model.history of
+        { hash } :: _ ->
+            if String.isEmpty hash then
+                testPoster
+            else
+                postContainer hash
+
+        [] ->
+            testPoster
+
+
+testPoster =
+    div []
+        (List.repeat 5 (postPoster "testpost" "This is a post title" "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet."))
+
+
+postContainer : String -> Html Msg
+postContainer id =
+    section [ class [ Style.Post ] ]
+        [ article []
+            [ text id
+            ]
+        ]
+
+
+postPoster : String -> String -> String -> Html Msg
+postPoster id title abstract =
+    a [ href ("#" ++ id) ]
+        [ section [ class [ Style.PostPoster ] ]
+            [ header [ class [ Style.PostPosterTitle ] ]
+                [ h2 [] [ text title ] ]
+            , article [ class [ Style.PostAbstract ] ]
+                [ text abstract ]
+            ]
+        ]
+
+
 view : Model -> Html Msg
 view model =
     div [ class [ Style.Container ] ]
         [ div [ class [ Style.ProfilePicture ] ] []
-        , h1 [] [ text "This is alpox' blog" ]
+        , h1 [] [ text "This is alpox's blog" ]
+        , postContent model
+        , footer []
+            [ div [ class [ Style.SocialLinks ] ]
+                [ a [ href "https://github.com/alpox" ]
+                    [ img [ src "img/github.png" ] []
+                    ]
+                ]
+            ]
         ]
--- src/Style.elm
+++ src/Style.elm
@@ -1,13 +1,24 @@
 module Style exposing (..)
 
 import Css exposing (..)
-import Css.Elements exposing (html, body, h1, li)
+import Css.Elements exposing (html, body, h1, h2, h3, h4, footer, li, a)
 import Css.Namespace exposing (namespace)
+
 
 type CssClasses
     = PageHost
     | Container
     | ProfilePicture
+    | Post
+    | PostPoster
+    | PostPosterTitle
+    | PostAbstract
+    | SocialLinks
+
+
+footerSize : Float
+footerSize = 100
+
 
 css : Stylesheet
 css =
@@ -19,16 +30,42 @@ css =
             , height (pct 100)
             ]
         , h1
-            [ textAlign center ]
+            [ textAlign center
+            ]
+        , each [ h1, h2, h3, h4 ]
+            [ color (rgb 40 40 40)
+            , fontFamilies [ "Philosopher", .value sansSerif ]
+            ]
+        , footer
+            [ position absolute
+            , bottom (px 0)
+            , left (px 0)
+            , property "box-shadow" "inset 0px 10px 10px -10px #222"
+            , width (pct 100)
+            , height (px footerSize)
+            , boxSizing borderBox
+            , backgroundColor (rgb 245 245 245)
+            ]
+        , (.) Post
+            []
         , (.) PageHost
             [ height (pct 100)
             ]
         , (.) Container
-            [ width (pct 80)
-            , height (pct 100)
+            [ position relative
+            , width (pct 80)
+            , minHeight (pct 100)
             , margin auto
-            , padding2 (px 20) (px 10)
+            , padding2 (px 20) (px 32)
+            , paddingBottom (px (footerSize + 32))
+              -- Make space for absolute positioned footer
             , backgroundColor (rgba 220 220 220 0.5)
+            , descendants
+                [ a
+                    [ textDecoration none
+                    , color initial
+                    ]
+                ]
             ]
         , (.) ProfilePicture
             [ width (px 300)
@@ -37,5 +74,38 @@ css =
             , backgroundSize cover
             , margin auto
             , borderRadius (pct 100)
+            ]
+        , (.) PostPoster
+            [ margin auto
+            , marginTop (px 50)
+            , padding2 (px 4) (px 20)
+            , width (pct 100)
+            , boxSizing borderBox
+            , boxShadow3 (px 1) (px 1) (px 4)
+            , borderRadius (px 4)
+            , overflow hidden
+            , backgroundColor (hex "fff")
+            , cursor pointer
+            , hover
+                [ boxShadow3 (px 1) (px 1) (px 8)
+                ]
+            , children
+                [ (.) PostPosterTitle
+                    [ borderBottom3 (px 1) solid (rgb 200 200 200)
+                    ]
+                , (.) PostAbstract
+                    [ paddingTop (px 16)
+                    , paddingBottom (px 16)
+                    ]
+                ]
+            ]
+        , (.) SocialLinks
+            [ position relative
+            , margin auto
+            , width (px 0)
+              -- Cheat for making the div not expand
+            , top (pct 50)
+            , transform (translateY (pct -50))
+            , children [ a [ display block ] ]
             ]
         ]
