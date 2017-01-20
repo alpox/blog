f920231e34093218b352702c6779a643794bdf51
alpoxo <ebernhaut@itbernhaut.ch>
2017-01-20 17:09:25 +0100

    Moved one level out

---
+++ deps/fs/c_src/mac/compat.d
@@ -0,0 +1,3 @@
+/Users/Elias/Projekte/bbs/web/deps/fs/c_src/mac/compat.o: \
+  /Users/Elias/Projekte/bbs/web/deps/fs/c_src/mac/compat.c \
+  /Users/Elias/Projekte/bbs/web/deps/fs/c_src/mac/compat.h
---
+++ deps/fs/c_src/mac/main.d
@@ -0,0 +1,5 @@
+/Users/Elias/Projekte/bbs/web/deps/fs/c_src/mac/main.o: \
+  /Users/Elias/Projekte/bbs/web/deps/fs/c_src/mac/main.c \
+  /Users/Elias/Projekte/bbs/web/deps/fs/c_src/mac/common.h \
+  /Users/Elias/Projekte/bbs/web/deps/fs/c_src/mac/compat.h \
+  /Users/Elias/Projekte/bbs/web/deps/fs/c_src/mac/cli.h
